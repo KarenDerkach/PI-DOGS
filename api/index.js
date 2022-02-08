f5ba35d7960cc3f18d04a6b7ea40b830f8354bbf
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-02-08 15:21:41 -0300

    add dontenv

--- api/index.js
+++ api/index.js
@@ -19,6 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
